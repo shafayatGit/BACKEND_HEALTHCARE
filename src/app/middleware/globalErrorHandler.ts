23d421e58920e9b01d9401ade558c0c1ad54d570
shafayatGit <shafayathossain.ielts@gmail.com>
2026-05-21 15:22:51 +0600

    feat: manually image upload in cloudinary

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -5,8 +5,9 @@ import z from "zod";
 import { TErrorResponse, TErrrorSources } from "../interfaces/error.interface";
 import { handleZodError } from "../errorHelpers/handleZodError";
 import AppError from "../errorHelpers/AppError";
+import { deleteFileFromCloudinary } from "../config/cloudinary.config";
 
-const globarErrorHandler = (
+const globarErrorHandler = async (
   err: any,
   req: Request,
   res: Response,
@@ -16,6 +17,14 @@ const globarErrorHandler = (
     console.log("Error from Global Error Handler: ", err);
   }
 
+  if (req.file) {
+    await deleteFileFromCloudinary(req.file.path);
+  }
+
+  if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+    const imageUrls = req.files.map((file) => file.path);
+    await Promise.all(imageUrls.map((url) => deleteFileFromCloudinary(url)));
+  }
   let errorSources: TErrrorSources[] = [];
 
   let statusCode: number = status.INTERNAL_SERVER_ERROR;
